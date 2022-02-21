59e3a70f34c5c40cfeeaf181b03737aa9175214a
kiran04121997 <bhosalekiran259@gmail.com>
2022-02-21 10:01:03 +0530

    [kiran] Add the Tictactoe game

--- Tic_Tac_ToeProject/Tic_Tac_ToeProject/Program.cs
+++ Tic_Tac_ToeProject/Tic_Tac_ToeProject/Program.cs
@@ -11,6 +11,8 @@ namespace Tic_Tac_ToeProject
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Tic_Tac_Toe_Game");
+            TicTacToeGame obj = new TicTacToeGame();
+            obj.gameBoard();
             Console.ReadLine();
         }
     }
---
+++ Tic_Tac_ToeProject/Tic_Tac_ToeProject/TicTacToeGame.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tic_Tac_ToeProject
+{
+    internal class TicTacToeGame
+    {
+        char[] Board = new char[10];
+        public void gameBoard()
+        {
+            for (int i = 1 ; i <9 ; i++)
+            {
+                Board[i] = '1';
+            }
+        }
+    }
+}
