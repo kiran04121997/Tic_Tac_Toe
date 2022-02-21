9829699890e3b50d7254d8f360774917ae7e69b1
kiran04121997 <bhosalekiran259@gmail.com>
2022-02-21 11:00:49 +0530

    [kiran] ability to input

--- Tic_Tac_ToeProject/Tic_Tac_ToeProject/TicTacToeGame.cs
+++ Tic_Tac_ToeProject/Tic_Tac_ToeProject/TicTacToeGame.cs
@@ -8,12 +8,36 @@ namespace Tic_Tac_ToeProject
 {
     internal class TicTacToeGame
     {
+        public const char X = 'X';
+        public const char O = 'O';
+
         char[] Board = new char[10];
         public void gameBoard()
         {
-            for (int i = 1 ; i <9 ; i++)
+
+            for (int i = 1; i < 9; i++)
             {
-                Board[i] = 'i';
+                Board[i] = ' ';
+            }
+        }
+
+        public void abillityToInput()
+        {
+            Console.WriteLine("Enter you Choice from O/X :");
+            char playerChoice = Convert.ToChar(Console.ReadLine());
+            if (playerChoice == 'X')
+            {
+                Console.WriteLine("Player Chose " + playerChoice);
+                Console.WriteLine("Computer Chose " + O);
+            }
+            else if (playerChoice == 'O')
+            {
+                Console.WriteLine("Player Chose " + playerChoice);
+                Console.WriteLine(" Computer Chose " + X);
+            }
+            else
+            {
+                Console.WriteLine("Entered invalid input  ");
             }
         }
     }
